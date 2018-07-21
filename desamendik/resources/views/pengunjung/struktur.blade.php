804818f59c46785560ac9cd17a68d5c4b56ef006
Mahathir Muhammad <Mahathirmuhammad02@gmail.com>
2018-07-22 02:03:26 +0800

    masih hal yg sama #part2

--- desamendik/resources/views/pengunjung/struktur.blade.php
+++ desamendik/resources/views/pengunjung/struktur.blade.php
@@ -1,13 +1,12 @@
 @extends('master')
 @section('content')
 
-
   <div class="container" style="width: 90%;">
     <div class="struktur">
       <div class="box">
         <div class="header-kegiatanlain">
           <div class="box-header">
-            <h3> <b>Struktur Organisasi Desa Mendik</b> </h3>
+            <h3> <b>Struktur Organisasi Desa Konoha Gakure</b> </h3>
           </div>
         </div>
         <div class="box-body">
@@ -16,17 +15,32 @@
               <div class="col-xs-4 col-lg-2">
                 <div class="box-img">
                   <div class="container-inner">
-                    <img src="{{asset('/img/Ade.jpg')}}" alt="Kades" class="image">
+                    <!-- <img src="{{asset('/img/Ade.jpg')}}" alt="Kades" class="image"> -->
+
+                    <figure>
+                      <div class="imgss"></div>
+
+                      <figcaption>
+                        <span></span>
+                        <span></span>
+                        <span>Senju Hashirama </br>Clan Bugis</span>
+                      </figcaption>
+
+                      <button></button>
+
                     <div class="overlay">
                       <div class="text">
-                        <p>Ade Widyatama Dian Boernama</p>
-                        <p>Mahasiswa KKN</p>
+                        <p>HOKAGA PERTAMA</p>
+                        <p>PEMIMPIN DESA KONOHA</p>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-              <div class="col-xs-4 col-lg-2">
+              </figure>
+              <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
+              <script  src="js/struktur.js"></script>
+              <!-- <div class="col-xs-4 col-lg-2">
                 <div class="box-img">
                   <div class="container-inner">
                     <img src="{{asset('/img/thumb-1920-411820.jpg')}}" alt="Kades" class="image">
@@ -98,8 +112,8 @@
                     </div>
                   </div>
                 </div>
-              </div>
-              <div class="col-xs-4 col-lg-2">
+              </div> -->
+              <!-- <div class="col-xs-4 col-lg-2">
                 <div class="box-img">
                   <div class="container-inner">
                     <img src="{{asset('/img/thumb-1920-411820.jpg')}}" alt="Kades" class="image">
@@ -111,7 +125,7 @@
                     </div>
                   </div>
                 </div>
-              </div>
+              </div> -->
             </div>
           </div>
         </div>
